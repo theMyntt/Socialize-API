e0e1e9d8d4ddca248cbf56b619f6f565ca3a722f
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-07-13 23:10:20 -0300

    chore: setting up Publication schema

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Socialize.Models;
+
+namespace Socialize.Data
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public ApplicationDbContext(DbContextOptions options) : base(options)
+		{
+
+		}
+
+		public DbSet<Publication> Publication { get; set; } 
+	}
+}
+
