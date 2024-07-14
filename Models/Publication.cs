87da638dd76785ca59f945453f99dbd5ca7a41ba
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-07-13 23:04:24 -0300

    feat: publication model

---
+++ Models/Publication.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Socialize.Models
+{
+	public class Publication
+	{
+		public int Id { get; set; }
+		public required string Text { get; set; }
+		public DateTime CreatedAt { get; set; }
+	}
+}
+
