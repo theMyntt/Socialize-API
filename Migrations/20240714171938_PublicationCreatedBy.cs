3132c3601163da7984f30f1b6f8950b257d0913e
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-07-14 14:24:24 -0300

    ci: migration of CreatedBy field to database

---
+++ Migrations/20240714171938_PublicationCreatedBy.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Socialize.Migrations
+{
+    /// <inheritdoc />
+    public partial class PublicationCreatedBy : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CreatedById",
+                table: "Publication",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Publication_CreatedById",
+                table: "Publication",
+                column: "CreatedById");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Publication_User_CreatedById",
+                table: "Publication",
+                column: "CreatedById",
+                principalTable: "User",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Publication_User_CreatedById",
+                table: "Publication");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Publication_CreatedById",
+                table: "Publication");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedById",
+                table: "Publication");
+        }
+    }
+}
