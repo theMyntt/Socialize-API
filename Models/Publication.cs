a49bcc70efe5e1bc7584ed63874292fe6ff45b62
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-07-14 00:19:45 -0300

    chore: annotation for identity and primary key

--- Models/Publication.cs
+++ Models/Publication.cs
@@ -1,8 +1,13 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Socialize.Models
 {
 	public class Publication
 	{
+		[Key]
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
 		public int Id { get; set; }
 		public required string Text { get; set; }
 		public DateTime CreatedAt { get; set; }
