0b59ae639d23833ecdd945bcb524570943b4d319
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-07-21 17:30:18 -0300

    chore: user aggregate

---
+++ Domain/Aggregates/UserAggregate.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace Socialize.Domain.Aggregates
+{
+	public class UserAggregate
+	{
+        public int? Id { get; set; }
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string? Description { get; set; }
+        public string Photo { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public UserAggregate(
+            int? id,
+            string name,
+            string code,
+            string email,
+            string password,
+            string? description,
+            string photo,
+            DateTime? createdAt
+        ) {
+            Id = id;
+            Code = code;
+            Email = email;
+            Name = name;
+            Password = password;
+            Description = description;
+            Photo = photo;
+            CreatedAt = createdAt ?? DateTime.Now;
+        }
+    }
+}
+
