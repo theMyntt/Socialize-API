0e16e0822a75355e48029027c31c6b1eef132355
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-07-21 17:30:35 -0300

    chore: user mapper to persistance and domain

---
+++ Infrastructure/Repositories/UserMapper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Socialize.Core;
+using Socialize.Domain.Aggregates;
+using Socialize.Models;
+
+namespace Socialize.Infrastructure.Repositories
+{
+    public class UserMapper : IMapperService<UserAggregate, User>
+    {
+        public UserAggregate toDomain(User target)
+        {
+            return new UserAggregate(target.Id, target.Name, target.Code, target.Email, target.Password, target.Description, target.Photo, target.CreatedAt); 
+        }
+
+        public User toPersistance(UserAggregate schema)
+        {
+            return new User(schema.Id ?? 1, schema.Name, schema.Code, schema.Email, schema.Password, schema.Description, schema.Photo, schema.CreatedAt);
+        }
+    }
+}
+
